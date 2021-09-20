1f7305f723a27a95dbb08f3258fd9e547057d5f4
Joel Goodman <joel@braverymedia.co>
2021-09-20 16:33:23 -0500

    setup everything

---
+++ .eleventy.js
@@ -0,0 +1,49 @@
+const yaml = require("js-yaml");
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+    // Disable automatic use of your .gitignore
+    eleventyConfig.setUseGitIgnore(false);
+
+    // To Support .yaml Extension in _data for Netlify CMS
+    eleventyConfig.addDataExtension("yaml", (contents) =>
+        yaml.safeLoad(contents)
+    );
+
+    // Copy Static Files to /_Site
+    eleventyConfig.addPassthroughCopy({
+        "./src/admin/config.yml": "./admin/config.yml"
+    });
+
+    // Minify HTML
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+
+        return content;
+    });
+
+    // Let Eleventy transform HTML files as nunjucks
+    // So that we can use .html instead of .njk
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid",
+            "yml"
+        ],
+        dir: {
+            input: "src",
+            includes: "src/_includes",
+            data: "src/_data",
+            output: "_site"
+        }
+    };
+}
